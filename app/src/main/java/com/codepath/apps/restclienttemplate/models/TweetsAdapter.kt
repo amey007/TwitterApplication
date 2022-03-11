b60fd31786eddf20d64fbc86ee8b948d96ecf6e7
amey007 <ameynarvekar01@gmail.com>
2022-03-11 11:19:51 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetsAdapter.kt
@@ -0,0 +1,46 @@
+package com.codepath.apps.restclienttemplate.models
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.codepath.apps.restclienttemplate.R
+import org.w3c.dom.Text
+
+class TweetsAdapter(val tweets: List<Tweet>) : RecyclerView.Adapter<TweetsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsAdapter.ViewHolder {
+        // Responsible for infalting the items in the tweets list
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        //Inflate our item layout
+        val view = inflater.inflate(R.layout.item_tweet, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // Populating data into the item using the View Holder
+    override fun onBindViewHolder(holder: TweetsAdapter.ViewHolder, position: Int) {
+        // get the data based on position
+        val tweet: Tweet = tweets.get(position)
+
+        // set item view based on views and data model
+        holder.tvUserName.text = tweet.user?.name   //? handles the null object
+        holder.tvTweetBody.text = tweet.body
+
+        Glide.with(holder.itemView).load(tweet.user?.publicImageUrl).into(holder.ivProfileImage)
+    }
+
+    override fun getItemCount(): Int {
+        return tweets.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val ivProfileImage = itemView.findViewById<ImageView>(R.id.ivProfileImage)
+        val tvUserName = itemView.findViewById<TextView>(R.id.tvUsername)
+        val tvTweetBody = itemView.findViewById<TextView>(R.id.tvTweetBody)
+    }
+}
